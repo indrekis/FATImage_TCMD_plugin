e8a16910e172e9ab39a787d76d7ec25f60e9fc65
indrekis <indrekis@gmail,com>
2022-07-22 02:54:13 +0300

    Copyright fixup

--- IMGwcx.cpp
+++ IMGwcx.cpp
@@ -1,17 +1,16 @@
 // This is a personal academic project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 /*
-IMG unpack plugin
-Copyright (C) 2002 IvGzury
-	ivgzury@hotmail.com
-
-This program is absolutely free software.
-If you have any remarks or problems, please don't
-hesitate to send me an email.
-
-It was made for Windows Commander.
-Windows Commander: is an excellent file manager
-				   made by Christian Ghisler
+* Floppy disk images unpack plugin for the Total Commander.
+* Copyright (c) 2002, IvGzury ( ivgzury@hotmail.com )
+* Copyright (c) 2022, Oleg Farenyuk aka Indrekis ( indrekis@gmail.com )
+*
+* Oleg Farenyuk's code is released under the MIT License.
+* 
+* Original IvGzury copyright message:
+* This program is absolutely free software.
+* If you have any remarks or problems, please don't
+* hesitate to send me an email.
 */
 
 #pragma pvs(disable: 2005,2001,303)
