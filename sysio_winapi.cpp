4a9a711d0ed3116cf283912cf63dab6e7235ce28
indrekis <indrekis@gmail,com>
2022-07-24 08:37:10 +0300

    Added copyright notices to cpp files.

--- sysio_winapi.cpp
+++ sysio_winapi.cpp
@@ -1,3 +1,17 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+/*
+* Floppy disk images unpack plugin for the Total Commander.
+* Copyright (c) 2002, IvGzury ( ivgzury@hotmail.com )
+* Copyright (c) 2022, Oleg Farenyuk aka Indrekis ( indrekis@gmail.com )
+*
+* Oleg Farenyuk's code is released under the MIT License.
+*
+* Original IvGzury copyright message:
+* This program is absolutely free software.
+* If you have any remarks or problems, please don't
+* hesitate to send me an email.
+*/
 #include "sysio_winapi.h"
 
 file_handle_t open_file_shared_read(const char* filename) {
